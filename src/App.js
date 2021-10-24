00382adb890678078d158944e305f4fe877fd63e
Morfeuschannel <morfeuschannel@gmail.com>
2021-10-24 15:43:46 +0300

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { View, ScreenSpinner, ConfigProvider, AdaptivityProvider, AppRoot, Panel, PanelHeader, SimpleCell, Header, Text, Group, CardGrid, CardScroll, Card, Avatar, Div } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
+import ivan from './img/ivan.jpg';
+import katya from './img/katya.jpg';
+import nastya from './img/nastya.jpg';
+import niyaz from './img/niyaz.jpg';
+
 
 function App() {
+
+  useEffect(() => {
+		const schemeAttribute = document.createAttribute('scheme');
+		schemeAttribute.value = 'bright_light';
+
+	}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider>
+			<AdaptivityProvider>
+				<AppRoot>
+					<View>
+						
+            <Panel>
+              <PanelHeader>
+                <Header subtitle="Команда Вездекода 2021">«Без булдырабыз»</Header>
+              </PanelHeader>
+              <Group>
+
+                <CardGrid size="l" >
+                  <Card mode="outline">
+                    <SimpleCell
+                      description="22 года"
+                      before={<Avatar src={ivan}/>}
+                      href="https://vk.com/ivstepin"
+                    >
+                      Иван Степин
+                    </SimpleCell>
+                    <Div>
+                      <Text>Бывший дизайнер. 1,5 года занимается веб-разработкой, пишет музыку, ищет тяночку и готовит борщи.</Text>
+                    </Div>
+                  </Card>
+
+                  <Card mode="outline">
+                    <SimpleCell
+                      description="18 лет"
+                      before={<Avatar src={katya}/>}
+                      href="https://vk.com/wish_nya1"
+                    >
+                      Анастасия Лаврова
+                    </SimpleCell>
+                    <Div>
+                      <Text>Настоящий дизайнер. Создаёт интерфейсы, однажды нарисовала кнопку в виде чак-чака, любит спать.</Text>
+                    </Div>
+                  </Card>
+
+                  <Card mode="outline">
+                    <SimpleCell
+                      description="29 лет"
+                      before={<Avatar src={nastya}/>}
+                      href="https://vk.com/one_step_cl0ser"
+                    >
+                      Екатерина Козлова
+                    </SimpleCell>
+                    <Div>
+                      <Text>Frontend разработчик. Написала свой первый сайт в блокноте. Теперь может вызвать дьявола через Webstorm.</Text>
+                    </Div>
+                  </Card>
+
+                  <Card mode="outline">
+                    <SimpleCell
+                      description="18 лет"
+                      before={<Avatar src={niyaz}/>}
+                      href="https://vk.com/lniaz"
+                    >
+                      Нияз Леушкин
+                    </SimpleCell>
+                    <Div>
+                      <Text>Прогает на си, java и c#. Любит собак и котиков. Заставил микроволновку думать, что она метеостанция.</Text>
+                    </Div>
+                  </Card>
+
+                </CardGrid>
+
+              </Group>
+            </Panel>
+
+					</View>
+				</AppRoot>
+			</AdaptivityProvider>
+		</ConfigProvider>
   );
 }
 
